ecf958b05388378db2fa4d5f24a201a4c562cfd8
Anna Choi <annachoi@bu.edu>
2018-01-24 16:57:25 -0500

    Add files via upload

---
+++ dodger/dodger.py
@@ -0,0 +1,201 @@
+import pygame, random, sys
+from pygame.locals import *
+
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+TEXTCOLOR = (255, 255, 255)
+BACKGROUNDCOLOR = (0, 0, 0)
+FPS = 40
+CORGIMINSIZE = 30
+CORGIMAXSIZE = 50
+CORGIMINSPEED = 1
+CORGIMAXSPEED = 8
+ADDNEWCORGIRATE = 6
+PLAYERMOVERATE = 5
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE: # pressing escape quits
+                    terminate()
+                return
+
+def playerHasHitCorgi(playerRect, corgis):
+    for c in corgis:
+        if playerRect.colliderect(c['rect']):
+            return True
+    return False
+
+def drawText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+# set up pygame, the window, and the mouse cursor
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('Dodger')
+pygame.mouse.set_visible(False)
+
+# set up fonts
+font = pygame.font.SysFont(None, 48)
+
+# change sounds
+gameOverSound = pygame.mixer.Sound('gameover.wav')
+pygame.mixer.music.load('background.mid')
+
+# set up images
+playerImage = pygame.image.load('player.png')
+playerRect = playerImage.get_rect()
+#need to change corgi image here
+corgiImage = pygame.image.load('corgi.png')
+
+# show the "Start" screen
+drawText('Dodger', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+drawText('Press a key to start.', font, windowSurface, (WINDOWWIDTH / 3) - 30, (WINDOWHEIGHT / 3) + 50)
+pygame.display.update()
+waitForPlayerToPressKey()
+
+
+topScore = 0
+while True:
+    # set up the start of the game
+    corgi = []
+    score = 0
+    playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 50)
+    moveLeft = moveRight = moveUp = moveDown = False
+    reverseCheat = slowCheat = False
+    corgiAddCounter = 0
+    pygame.mixer.music.play(-1, 0.0)
+
+    while True: # the game loop runs while the game part is playing
+        score += 1 # increase score
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+
+            if event.type == KEYDOWN:
+                if event.key == ord('z'):
+                    reverseCheat = True
+                if event.key == ord('x'):
+                    slowCheat = True
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveRight = False
+                    moveLeft = True
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveLeft = False
+                    moveRight = True
+                if event.key == K_UP or event.key == ord('w'):
+                    moveDown = False
+                    moveUp = True
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveUp = False
+                    moveDown = True
+
+            if event.type == KEYUP:
+                if event.key == ord('z'):
+                    reverseCheat = False
+                    score = 0
+                if event.key == ord('x'):
+                    slowCheat = False
+                    score = 0
+                if event.key == K_ESCAPE:
+                        terminate()
+
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveRight = False
+                if event.key == K_UP or event.key == ord('w'):
+                    moveUp = False
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveDown = False
+
+            if event.type == MOUSEMOTION:
+                # If the mouse moves, move the player where the cursor is.
+                playerRect.move_ip(event.pos[0] - playerRect.centerx, event.pos[1] - playerRect.centery)
+
+        # Add new corgis at the top of the screen, if needed.
+        if not reverseCheat and not slowCheat:
+            corgiAddCounter += 1
+        if corgiAddCounter == ADDNEWCORGIRATE:
+            corgiAddCounter = 0
+            corgiSize = random.randint(CORGIMINSIZE, CORGIMAXSIZE)
+            newcorgi = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-corgiSize), 0 - corgiSize, corgiSize, corgiSize),
+                        'speed': random.randint(CORGIMINSPEED, CORGIMAXSPEED),
+                        'surface':pygame.transform.scale(corgiImage, (corgiSize, corgiSize)),
+                        }
+
+            corgi.append(newcorgi)
+
+        # Move the player around.
+        if moveLeft and playerRect.left > 0:
+            playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
+        if moveRight and playerRect.right < WINDOWWIDTH:
+            playerRect.move_ip(PLAYERMOVERATE, 0)
+        if moveUp and playerRect.top > 0:
+            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
+        if moveDown and playerRect.bottom < WINDOWHEIGHT:
+            playerRect.move_ip(0, PLAYERMOVERATE)
+
+        # Move the mouse cursor to match the player.
+        pygame.mouse.set_pos(playerRect.centerx, playerRect.centery)
+
+        # Move the corgis down.
+        for c in corgi:
+            if not reverseCheat and not slowCheat:
+                c['rect'].move_ip(0, c['speed'])
+            elif reverseCheat:
+                c['rect'].move_ip(0, -5)
+            elif slowCheat:
+                c['rect'].move_ip(0, 1)
+                
+         # Delete corgis that have fallen past the bottom.
+        for c in corgi[:]:
+            if c['rect'].top > WINDOWHEIGHT:
+                corgi.remove(c)
+
+        # Draw the game world on the window.
+        windowSurface.fill(BACKGROUNDCOLOR)
+
+        # Draw the score and top score.
+        drawText('Score: %s' % (score), font, windowSurface, 10, 0)
+        drawText('Top Score: %s' % (topScore), font, windowSurface, 10, 40)
+
+        # Draw the player's rectangle
+        windowSurface.blit(playerImage, playerRect)
+
+        # Draw each corgi
+        for c in corgi:
+            windowSurface.blit(c['surface'], c['rect'])
+
+        pygame.display.update()
+
+        # Check if any of the corgis have hit the player.
+        if playerHasHitCorgi(playerRect, corgi):
+            if score > topScore:
+                topScore = score # set new top score
+            break
+
+        mainClock.tick(FPS)
+
+    # Stop the game and show the "Game Over" screen.
+    pygame.mixer.music.stop()
+    gameOverSound.play()
+
+    drawText('GAME OVER', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+    drawText('Press a key to play again.', font, windowSurface, (WINDOWWIDTH / 3) - 80, (WINDOWHEIGHT / 3) + 50)
+    pygame.display.update()
+    waitForPlayerToPressKey()
+
+    gameOverSound.stop()
